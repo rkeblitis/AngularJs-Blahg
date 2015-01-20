cb043a25d8831fa2289baf5a7744bdb456795fbb
Rachelle Keblitis <rkeblitis@gmail.com>
2015-01-20 14:01:07 -0800

    first very late commit

---
+++ app/js/controllers/layout.js
@@ -0,0 +1,5 @@
+var homeControllerModule = angular.module('homeControllerModule', []);
+
+homeControllerModule.controller('homeController', ['$scope', '$http', function($scope, $http) {
+  $scope.hello = "Hi";
+}]);
