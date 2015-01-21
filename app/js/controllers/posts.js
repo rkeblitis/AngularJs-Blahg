25942d2548eb5e0699b544ae6b410c893229a9cc
Rachelle Keblitis <rkeblitis@gmail.com>
2015-01-20 16:22:12 -0800

    hook up api

--- app/js/controllers/posts.js
+++ app/js/controllers/posts.js
@@ -2,6 +2,9 @@ var postsControllerModule = angular.module('postsControllerModule', []);
 
 postsControllerModule.controller('postsController', ['$scope', '$http', function($scope, $http) {
   $scope.name = "Posts Controller"
+  $http.get('http://localhost:3000/posts').success(function(data) {
+    $scope.posts = data;
+  });
 }]);
 
 postsControllerModule.controller('newPostController', ['$scope', '$http', function($scope, $http) {
